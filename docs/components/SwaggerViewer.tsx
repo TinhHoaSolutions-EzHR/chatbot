81569e6f63438884d4dac711873a240629bcf5e6
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-11-18 21:03:43 +0700

    feat(docs): add new dependencies for Swagger UI and improve component imports

--- docs/components/SwaggerViewer.tsx
+++ docs/components/SwaggerViewer.tsx
@@ -3,8 +3,6 @@ import dynamic from "next/dynamic";
 // Dynamically import SwaggerUI to avoid SSR issues
 const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
-import "swagger-ui-react/swagger-ui.css";
-
 const SwaggerViewer = () => {
     return <SwaggerUI url="/assets/api-docs/openapi.yaml" />;
 };
