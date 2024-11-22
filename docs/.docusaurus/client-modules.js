4fe60c892ffaa115ad5d2ea1f972c4297ead5aea
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-11-22 21:12:05 +0700

    feat(docs): initialize documentation structure with new sidebar, local development guide, and configuration files

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/nho/Documents/ezhr/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/nho/Documents/ezhr/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/nho/Documents/ezhr/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/nho/Documents/ezhr/docs/src/css/custom.css"),
-  require("/home/nho/Documents/ezhr/docs/node_modules/docusaurus-theme-openapi-docs/lib/theme/styles.scss"),
-];
