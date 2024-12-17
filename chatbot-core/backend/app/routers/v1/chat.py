c3166848769a41f09a3676f152f5723aa2797925
tuantran0910 <tntuan0910@gmail.com>
2024-12-17 23:52:37 +0700

    fix(backend): make sure user cannot be none on endpoints

--- chatbot-core/backend/app/routers/v1/chat.py
+++ chatbot-core/backend/app/routers/v1/chat.py
@@ -27,20 +27,20 @@ router = APIRouter(prefix="/chat", tags=["chat", "session", "message"])
 
 
 @router.get("/chat-sessions", response_model=APIResponse, status_code=status.HTTP_200_OK)
-def get_chat_sessions(
-    db_session: Session = Depends(get_db_session), user: User | None = Depends(get_current_user)
-) -> None:
+def get_chat_sessions(db_session: Session = Depends(get_db_session), user: User = Depends(get_current_user)) -> None:
     """
     Get all chat sessions of the user.
 
     Args:
         db_session (Session): Database session. Defaults to relational database session.
-        user (User | None): User object
+        user (User): User object
     """
-    user_id = user.id if user else None
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
 
     # Get chat sessions of user
-    chat_sessions, err = ChatService(db_session=db_session).get_chat_sessions(user_id=user_id)
+    chat_sessions, err = ChatService(db_session=db_session).get_chat_sessions(user_id=user.id)
     if err:
         status_code, detail = err.kind
         raise HTTPException(status_code=status_code, detail=detail)
@@ -58,7 +58,7 @@ def get_chat_sessions(
 def get_chat_session(
     chat_session_id: str,
     db_session: Session = Depends(get_db_session),
-    user: User | None = Depends(get_current_user),
+    user: User = Depends(get_current_user),
 ) -> None:
     """
     Get chat session by id.
@@ -66,13 +66,15 @@ def get_chat_session(
     Args:
         chat_session_id (str): Chat session id.
         db_session (Session): Database session. Defaults to relational database session.
-        user (User | None): User object.
+        user (User): User object.
     """
-    user_id = user.id if user else None
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
 
     # Get chat session
     chat_session, err = ChatService(db_session=db_session).get_chat_session(
-        chat_session_id=chat_session_id, user_id=user_id
+        chat_session_id=chat_session_id, user_id=user.id
     )
     if err:
         status_code, detail = err.kind
@@ -91,7 +93,7 @@ def get_chat_session(
 def create_chat_session(
     chat_session_request: ChatSessionRequest,
     db_session: Session = Depends(get_db_session),
-    user: User | None = Depends(get_current_user),
+    user: User = Depends(get_current_user),
 ) -> None:
     """
     Create chat session.
@@ -99,13 +101,15 @@ def create_chat_session(
     Args:
         chat_session_request (ChatSessionRequest): Chat session request object.
         db_session (Session): Database session. Defaults to relational database session.
-        user (User | None): User object.
+        user (User): User object.
     """
-    user_id = user.id if user else None
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
 
     # Create chat session
     err = ChatService(db_session=db_session).create_chat_session(
-        chat_session_request=chat_session_request, user_id=user_id
+        chat_session_request=chat_session_request, user_id=user.id
     )
     if err:
         status_code, detail = err.kind
@@ -122,7 +126,7 @@ def update_chat_session(
     chat_session_id: str,
     chat_session_request: ChatSessionRequest,
     db_session: Session = Depends(get_db_session),
-    user: User | None = Depends(get_current_user),
+    user: User = Depends(get_current_user),
 ) -> None:
     """
     Update chat session.
@@ -131,13 +135,15 @@ def update_chat_session(
         chat_session_id (str): Chat session id.
         chat_session_request (ChatSessionRequest): Chat session request object.
         db_session (Session): Database session. Defaults to relational database session.
-        user (User | None): User object.
+        user (User): User object.
     """
-    user_id = user.id if user else None
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
 
     # Update chat session
     err = ChatService(db_session=db_session).update_chat_session(
-        chat_session_id=chat_session_id, chat_session_request=chat_session_request, user_id=user_id
+        chat_session_id=chat_session_id, chat_session_request=chat_session_request, user_id=user.id
     )
     if err:
         status_code, detail = err.kind
@@ -153,7 +159,7 @@ def update_chat_session(
 def delete_chat_session(
     chat_session_id: str,
     db_session: Session = Depends(get_db_session),
-    user: User | None = Depends(get_current_user),
+    user: User = Depends(get_current_user),
 ) -> None:
     """
     Delete chat session.
@@ -161,12 +167,14 @@ def delete_chat_session(
     Args:
         chat_session_id (str): Chat session id.
         db_session (Session): Database session. Defaults to relational database session.
-        user (User | None): User object.
+        user (User): User object.
     """
-    user_id = user.id if user else None
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
 
     # Delete chat session
-    err = ChatService(db_session=db_session).delete_chat_session(chat_session_id=chat_session_id, user_id=user_id)
+    err = ChatService(db_session=db_session).delete_chat_session(chat_session_id=chat_session_id, user_id=user.id)
     if err:
         status_code, detail = err.kind
         raise HTTPException(status_code=status_code, detail=detail)
@@ -180,7 +188,7 @@ def handle_new_chat_message(
     chat_session_id: str,
     chat_message_request: ChatMessageRequest,
     db_session: Session = Depends(get_db_session),
-    user: User | None = Depends(get_current_user),
+    user: User = Depends(get_current_user),
 ) -> StreamingResponse:
     """
     This endpoint is both used for all the following purposes:
@@ -192,20 +200,22 @@ def handle_new_chat_message(
         chat_session_id (str): Chat session id.
         chat_message_request (ChatMessageRequest): Chat message request object.
         db_session (Session): Database session. Defaults to relational database session.
-        user (User | None): User object.
+        user (User): User object.
 
     Returns:
         StreamingResponse: Streams the response to the new chat message.
     """
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
+
     if chat_message_request.request_type != ChatMessageRequestType.REGENERATE and not chat_message_request.message:
         status_code, detail = ErrorCodesMappingNumber.EMPTY_CHAT_MESSAGE
         raise HTTPException(status_code=status_code, detail=detail)
 
-    user_id = user.id if user else None
-
     # Generate the content
     content = ChatService(db_session=db_session).generate_stream_chat_message(
-        chat_message_request=chat_message_request, chat_session_id=chat_session_id, user_id=user_id
+        chat_message_request=chat_message_request, chat_session_id=chat_session_id, user_id=user.id
     )
     if not isinstance(content, Generator):
         status_code, detail = content
