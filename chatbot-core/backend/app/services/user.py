b9f6b6aeac08f6d2f3060db636045a902e3c5e3a
tuantran0910 <tntuan0910@gmail.com>
2024-12-19 22:06:09 +0700

    fix(backend): bug not declare router and type of list to save in db

--- chatbot-core/backend/app/services/user.py
+++ chatbot-core/backend/app/services/user.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 from typing import Optional
 
@@ -41,17 +42,23 @@ class UserService(BaseService):
         if not recent_agent_ids:
             recent_agent_ids = []
         else:
-            recent_agent_ids = [agent_id for agent_id in recent_agent_ids if agent_id != current_agent_id]
+            recent_agent_ids = [
+                agent_id for agent_id in recent_agent_ids if agent_id != current_agent_id
+            ]
 
         # Add the current agent ID to the beginning of the list
+        logger.info(f"Adding current agent ID: {current_agent_id} to the beginning of the list")
         recent_agent_ids.insert(0, current_agent_id)
 
         # Limit the number of recent agents to 5
         recent_agent_ids = recent_agent_ids[:5]
+        logger.info(f"Current recent agent IDs: {recent_agent_ids}")
 
         return recent_agent_ids
 
-    def update_user_settings(self, user: User, user_setting_request: UserSettingRequest) -> Optional[APIError]:
+    def update_user_settings(
+        self, user: User, user_setting_request: UserSettingRequest
+    ) -> Optional[APIError]:
         """
         Update user settings.
 
@@ -64,15 +71,15 @@ class UserService(BaseService):
         """
         with self._transaction():
             # Handle update recent agents
-            recent_agent_ids = user.recent_agent_ids
+            recent_agent_ids = json.loads(user.recent_agent_ids) if user.recent_agent_ids else []
             if user_setting_request.current_agent_id:
-                current_agent_id = user_setting_request.current_agent_id
+                current_agent_id = str(user_setting_request.current_agent_id)
                 recent_agent_ids = self._handle_recent_agents(
                     recent_agent_ids=recent_agent_ids, current_agent_id=current_agent_id
                 )
 
                 # Update user recent agents
-                user.recent_agent_ids = recent_agent_ids
+                user.recent_agent_ids = json.dumps(recent_agent_ids)
 
             # Update other user settings
             user.auto_scroll = user_setting_request.auto_scroll
