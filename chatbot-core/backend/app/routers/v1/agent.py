e2a0b26e7f78da42080c8ea027d4faf0a7b0ebd2
tuantran0910 <tntuan0910@gmail.com>
2024-12-21 19:47:53 +0700

    feat(backend): initialize the agent endpoints

---
+++ chatbot-core/backend/app/routers/v1/agent.py
@@ -0,0 +1,209 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
+from sqlalchemy.orm import Session
+
+from app.databases.mssql import get_db_session
+from app.models import User
+from app.models.agent import AgentRequest
+from app.models.agent import AgentResponse
+from app.services.agent import AgentService
+from app.settings import Constants
+from app.utils.api.api_response import APIResponse
+from app.utils.api.api_response import BackendAPIResponse
+from app.utils.api.error_handler import ErrorCodesMappingNumber
+from app.utils.api.helpers import get_logger
+from app.utils.user.authentication import get_current_user
+
+
+logger = get_logger(__name__)
+router = APIRouter(prefix="/agents", tags=["agent", "assistant", "prompt"])
+
+
+@router.get("", response_model=APIResponse, status_code=status.HTTP_200_OK)
+def get_agents(
+    db_session: Session = Depends(get_db_session),
+    user: User = Depends(get_current_user),
+) -> BackendAPIResponse:
+    """
+    Get all agents.
+
+    Args:
+        db_session (Session, optional): Database session. Defaults to relational database engine.
+        user (User, optional): Current user. Defaults to get_current_user.
+
+    Returns:
+        BackendAPIResponse: API response with the list of agents.
+    """
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Get agents
+    agents, err = AgentService(db_session=db_session).get_agents(user_id=user.id)
+    if err:
+        status_code, detail = err.kind
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Parse agents
+    if agents:
+        data = [AgentResponse.model_validate(agent) for agent in agents]
+    else:
+        data = []
+
+    return (
+        BackendAPIResponse()
+        .set_message(message=Constants.API_SUCCESS)
+        .set_data(data=data)
+        .respond()
+    )
+
+
+@router.get("/{agent_id}", response_model=APIResponse, status_code=status.HTTP_200_OK)
+def get_agent(
+    agent_id: str,
+    db_session: Session = Depends(get_db_session),
+    user: User = Depends(get_current_user),
+) -> BackendAPIResponse:
+    """
+    Get an agent by id.
+
+    Args:
+        agent_id (str): Agent id
+        db_session (Session, optional): Database session. Defaults to relational database engine.
+        user (User, optional): Current user. Defaults to get_current_user.
+
+    Returns:
+        BackendAPIResponse: API response with the agent.
+    """
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Get agent
+    agent, err = AgentService(db_session=db_session).get_agent(agent_id=agent_id, user_id=user.id)
+    if err:
+        status_code, detail = err.kind
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Parse agent
+    if agent:
+        data = AgentResponse.model_validate(agent)
+    else:
+        data = None
+
+    return (
+        BackendAPIResponse()
+        .set_message(message=Constants.API_SUCCESS)
+        .set_data(data=data)
+        .respond()
+    )
+
+
+@router.post("", response_model=APIResponse, status_code=status.HTTP_201_CREATED)
+def create_agent(
+    agent_request: AgentRequest,
+    db_session: Session = Depends(get_db_session),
+    user: User = Depends(get_current_user),
+) -> BackendAPIResponse:
+    """
+    Create a new agent.
+
+    Args:
+        agent_request (AgentRequest): Agent request object.
+        db_session (Session, optional): Database session. Defaults to relational database engine.
+        user (User, optional): Current user. Defaults to get_current_user.
+
+    Returns:
+        BackendAPIResponse: API response with the created agent.
+    """
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Create agent
+    err = AgentService(db_session=db_session).create_agent(agent_request=agent_request)
+    if err:
+        status_code, detail = err.kind
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Parse agent
+    data = AgentRequest.model_dump(exclude_unset=True)
+
+    return (
+        BackendAPIResponse()
+        .set_message(message=Constants.API_SUCCESS)
+        .set_data(data=data)
+        .respond()
+    )
+
+
+@router.patch("/{agent_id}", response_model=APIResponse, status_code=status.HTTP_200_OK)
+def update_agent(
+    agent_id: str,
+    agent_request: AgentRequest,
+    db_session: Session = Depends(get_db_session),
+    user: User = Depends(get_current_user),
+) -> BackendAPIResponse:
+    """
+    Update an agent.
+
+    Args:
+        agent_id (str): Agent id
+        agent_request (AgentRequest): Agent request object.
+        db_session (Session, optional): Database session. Defaults to relational database engine.
+        user (User, optional): Current user. Defaults to get_current_user.
+
+    Returns:
+        BackendAPIResponse: API response with the updated agent.
+    """
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Update agent
+    err = AgentService(db_session=db_session).update_agent(
+        agent_id=agent_id, agent_request=agent_request, user_id=user.id
+    )
+    if err:
+        status_code, detail = err.kind
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Parse agent
+    data = AgentRequest.model_dump(exclude_unset=True)
+
+    return (
+        BackendAPIResponse()
+        .set_message(message=Constants.API_SUCCESS)
+        .set_data(data=data)
+        .respond()
+    )
+
+
+@router.delete("/{agent_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_agent(
+    agent_id: str,
+    db_session: Session = Depends(get_db_session),
+    user: User = Depends(get_current_user),
+) -> None:
+    """
+    Delete an agent.
+
+    Args:
+        agent_id (str): Agent id
+        db_session (Session, optional): Database session. Defaults to relational database engine.
+        user (User, optional): Current user. Defaults to get_current_user.
+
+    Returns:
+        None
+    """
+    if not user:
+        status_code, detail = ErrorCodesMappingNumber.UNAUTHORIZED_REQUEST.value
+        raise HTTPException(status_code=status_code, detail=detail)
+
+    # Delete agent
+    err = AgentService(db_session=db_session).delete_agent(agent_id=agent_id, user_id=user.id)
+    if err:
+        status_code, detail = err.kind
+        raise HTTPException(status_code=status_code, detail=detail)
---
+++ chatbot-core/backend/app/services/agent.py
@@ -0,0 +1,122 @@
+from typing import List
+from typing import Optional
+from typing import Tuple
+
+from sqlalchemy.orm import Session
+
+from app.models import Agent
+from app.models.agent import AgentRequest
+from app.repositories.agent import AgentRepository
+from app.services.base import BaseService
+from app.utils.api.api_response import APIError
+from app.utils.api.helpers import get_logger
+
+logger = get_logger(__name__)
+
+
+class AgentService(BaseService):
+    def __init__(self, db_session: Session):
+        """
+        Agent service class for handling agent-related operations.
+
+        Args:
+            db_session(Session): Database session
+        """
+        super().__init__(db_session)
+
+        self._agent_repo = AgentRepository(db_session=db_session)
+
+    def get_agents(self, user_id: str) -> Tuple[Optional[List[Agent]], Optional[APIError]]:
+        """
+        Get all agents of the user. Sort by display_priority.
+
+        Args:
+            user_id(str): User id
+
+        Returns:
+            Tuple[Optional[List[Agent]], Optional[APIError]]: List of agent objects and APIError object if any error
+        """
+        return self._agent_repo.get_agents(user_id=user_id)
+
+    def get_agent(self, agent_id: str, user_id: str) -> Tuple[Optional[Agent], Optional[APIError]]:
+        """
+        Get an agent by id.
+
+        Args:
+            agent_id(str): Agent id
+            user_id(str): User id
+
+        Returns:
+            Tuple[Optional[Agent], Optional[APIError]]: Agent object and APIError object if any error
+        """
+        return self._agent_repo.get_agent(agent_id=agent_id, user_id=user_id)
+
+    def create_agent(self, agent_request: AgentRequest) -> Optional[APIError]:
+        """
+        Create a new agent.
+
+        Args:
+            agent_request(AgentRequest): Agent request object
+
+        Returns:
+            Optional[APIError]: APIError object if any error
+        """
+        with self._transaction():
+            # Define agent
+            agent = Agent(
+                name=agent_request.name,
+                description=agent_request.description,
+                agent_type=agent_request.agent_type,
+                uploaded_image_id=agent_request.uploaded_image_id,
+            )
+
+            # Create agent
+            err = self._agent_repo.create_agent(agent=agent)
+
+        return err if err else None
+
+    def update_agent(
+        self, agent_id: str, agent_request: AgentRequest, user_id: str
+    ) -> Optional[APIError]:
+        """
+        Update an agent.
+
+        Args:
+            agent_id(str): Agent id
+            agent_request(AgentRequest): Agent request object
+            user_id(str): User id
+
+        Returns:
+            Optional[APIError]: APIError object if any error
+        """
+        with self._transaction():
+            # Define to-be-updated agent
+            agent = Agent(
+                name=agent_request.name,
+                description=agent_request.description,
+                is_visible=agent_request.is_visible,
+                display_priority=agent_request.display_priority,
+                uploaded_image_id=agent_request.uploaded_image_id,
+            )
+
+            # Update agent
+            err = self._agent_repo.update_agent(agent_id=agent_id, agent=agent, user_id=user_id)
+
+        return err if err else None
+
+    def delete_agent(self, agent_id: str, user_id: str) -> Optional[APIError]:
+        """
+        Delete an agent.
+
+        Args:
+            agent_id(str): Agent id
+            user_id(str): User id
+
+        Returns:
+            Optional[APIError]: APIError object if any error
+        """
+        with self._transaction():
+            # Delete agent
+            err = self._agent_repo.delete_agent(agent_id=agent_id, user_id=user_id)
+
+        return err if err else None
