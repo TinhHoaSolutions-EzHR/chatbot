1332237d1dcbe1465c15732bb124d49b3652a5a2
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-12-29 15:14:23 +0700

    fix(docker): FIX NGINX, REMOVE UNUSED CONFIG FILES

--- deployment/data/nginx/run-nginx.sh
+++ deployment/data/nginx/run-nginx.sh
@@ -1,17 +1,11 @@
 # fill in the template
 envsubst '$DOMAIN $SSL_CERT_FILE_NAME $SSL_CERT_KEY_FILE_NAME' <"/etc/nginx/conf.d/$1" >/etc/nginx/conf.d/app.conf
 
-# wait for the api_server to be ready
-echo "Waiting for API server to boot up; this may take a minute or two..."
-echo "If this takes more than ~5 minutes, check the logs of the API server container for errors with the following command:"
-echo
-echo "docker logs chatbot-core_api_server-1"
-echo
 
 while true; do
 	# Use curl to send a request and capture the HTTP status code
 	# NOTE: default port of api_server is 5000
-	status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "http://api_server:5000/health")
+	status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "http://api-server:5000/health")
 
 	# Check if the status code is 200
 	if [ "$status_code" -eq 200 ]; then
@@ -24,9 +18,9 @@ while true; do
 done
 
 # Start nginx and reload every 6 hours
-while :; do
-	sleep 6h &
-	wait
-	nginx -s reload
-done &
+# while :; do
+# 	sleep 6h &
+# 	wait
+# 	nginx -s reload
+# done &
 nginx -g "daemon off;"
