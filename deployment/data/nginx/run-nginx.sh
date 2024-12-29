136b4bd079818c38576730c7a6d2bb87cb084827
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-12-29 22:45:49 +0700

    refactor(nginx): simplify run-nginx.sh by removing unnecessary wait logic for API server

--- deployment/data/nginx/run-nginx.sh
+++ deployment/data/nginx/run-nginx.sh
@@ -1,12 +1,4 @@
-# fill in the template
 envsubst '$DOMAIN $SSL_CERT_FILE_NAME $SSL_CERT_KEY_FILE_NAME' <"/etc/nginx/conf.d/$1" >/etc/nginx/conf.d/app.conf
-
-# wait for the api_server to be ready
-echo "Waiting for API server to boot up; this may take a minute or two..."
-echo "If this takes more than ~5 minutes, check the logs of the API server container for errors with the following command:"
-echo
-echo "docker logs chatbot-core_api_server-1"
-echo
 
 while true; do
 	# Use curl to send a request and capture the HTTP status code
