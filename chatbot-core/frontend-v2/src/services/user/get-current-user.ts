1af261eb6c0ca50d7be6c5b507ba8c1407428639
greyy-nguyen <nhan.nguyen@skymavis.com>
2025-01-08 12:34:28 +0700

    feat(frontend): refactor frontend api call

--- chatbot-core/frontend-v2/src/services/user/get-current-user.ts
+++ chatbot-core/frontend-v2/src/services/user/get-current-user.ts
@@ -1,11 +1,11 @@
-import api from '@/lib/axios';
+import httpClient from '@/lib/axios';
 import { IApiResponse } from '@/types/api-response';
 import { IUser } from '@/types/user';
 import { ApiEndpointPrefix, getApiUrl } from '@/utils/get-api-url';
 
 export const getCurrentUser = async (): Promise<IUser | null> => {
   try {
-    const res = await api.get<IApiResponse<IUser>>(getApiUrl(ApiEndpointPrefix.USER));
+    const res = await httpClient.get<IApiResponse<IUser>>(getApiUrl(ApiEndpointPrefix.USER));
     return res.data.data;
   } catch (error) {
     console.error('[GetUser]: ', error);
