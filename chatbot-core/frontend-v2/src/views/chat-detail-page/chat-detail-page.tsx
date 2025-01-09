15e669baa6a3fe0436c09bc31880f1bc2fb88306
greyy-nguyen <nhan.nguyen@skymavis.com>
2025-01-09 15:04:25 +0700

    chore(frontend): remove testing elements

--- chatbot-core/frontend-v2/src/views/chat-detail-page/chat-detail-page.tsx
+++ chatbot-core/frontend-v2/src/views/chat-detail-page/chat-detail-page.tsx
@@ -1,36 +1,7 @@
 export default function ChatDetailPage() {
   return (
     <div className="chat-width h-full">
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
-      <div className="my-4">Hello world</div>
+      <div>Hello world</div>
     </div>
   );
 }
