af99fe1545bbde5f6cbadc69b5c0f9b81319982d
tuantran0910 <tntuan0910@gmail.com>
2025-01-18 11:05:10 +0700

    feat(backend): add more celery hooks

--- chatbot-core/backend/app/background/celery_worker.py
+++ chatbot-core/backend/app/background/celery_worker.py
@@ -1,11 +1,12 @@
 from typing import Any
 
 from celery import Celery
+from celery.signals import setup_logging
 from celery.signals import worker_init
+from celery.signals import worker_ready
+from celery.signals import worker_shutdown
 
-from app.background.utils import wait_for_db
-from app.background.utils import wait_for_qdrant
-from app.background.utils import wait_for_redis
+import app.background.utils as utils
 from app.utils.api.helpers import get_logger
 
 
@@ -29,9 +30,55 @@ def on_worker_init(sender: Any, **kwargs) -> None:
     logger.info("Worker initialization started")
 
     # Wait for the services to be ready
-    wait_for_redis(sender, **kwargs)
-    wait_for_db(sender, **kwargs)
-    wait_for_qdrant(sender, **kwargs)
+    utils.wait_for_redis(sender=sender, **kwargs)
+    utils.wait_for_db(sender=sender, **kwargs)
+    utils.wait_for_qdrant(sender=sender, **kwargs)
+
+
+@worker_ready.connect
+def on_worker_ready(sender: Any = None, **kwargs) -> None:
+    """
+    Handles worker ready signal. This runs when the worker is ready to process tasks,
+    after initialization is complete.
+
+    Args:
+        sender (Any): The sender of the signal.
+        **kwargs: Arbitrary keyword arguments.
+    """
+    utils.on_worker_ready(sender=sender, **kwargs)
+
+
+@worker_shutdown.connect
+def on_worker_shutdown(sender: Any = None, **kwargs) -> None:
+    """
+    Handles worker shutdown signal. This runs when the worker is shutting down.
+
+    Args:
+        sender (Any): The sender of the signal.
+        **kwargs: Arbitrary keyword arguments.
+    """
+    utils.on_worker_shutdown(sender=sender, **kwargs)
+
+
+@setup_logging.connect
+def on_setup_logging(loglevel: Any, logfile: Any, format: Any, colorize: Any, **kwargs) -> None:
+    """
+    Setup logging for the Celery worker.
+
+    Args:
+        loglevel (Any): The logging level.
+        logfile (Any): The file to log to.
+        format (Any): The log format.
+        colorize (Any): Whether to colorize the log.
+        **kwargs: Additional keyword arguments.
+    """
+    utils.on_setup_logging(
+        loglevel=loglevel,
+        logfile=logfile,
+        format=format,
+        colorize=colorize,
+        **kwargs,
+    )
 
 
 # Auto-discover tasks
