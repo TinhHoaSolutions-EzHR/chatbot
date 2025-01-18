e37ebd109c44660fac33f7ceccc158ce907ec926
tuantran0910 <tntuan0910@gmail.com>
2025-01-18 11:05:10 +0700

    feat(backend): add more celery hooks

--- chatbot-core/backend/app/background/utils.py
+++ chatbot-core/backend/app/background/utils.py
@@ -2,28 +2,36 @@ import time
 from typing import Any
 
 from celery.exceptions import WorkerShutdown
+from celery.utils.log import get_task_logger
 from redis.exceptions import RedisError
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 
 from app.background.helpers import ProbeConfig
 from app.background.helpers import timeout_context
-from app.databases.mssql import get_db_session
+from app.databases.mssql import MSSQLConnector
 from app.databases.qdrant import QdrantConnector
 from app.databases.redis import RedisConnector
 from app.utils.api.error_handler import RedisProbeError
+from app.utils.api.helpers import CeleryTaskColoredFormatter
+from app.utils.api.helpers import CeleryTaskPlainFormatter
+from app.utils.api.helpers import ColoredFormatter
 from app.utils.api.helpers import get_logger
+from app.utils.api.helpers import PlainFormatter
 
 
 logger = get_logger(__name__)
+task_logger = get_task_logger(__name__)
 
 
-def wait_for_redis(sender: Any, **kwargs) -> None:
+def wait_for_redis(sender: Any, **kwargs) -> None:  # NOSONAR
     """
     Wait for Redis to be available before starting the Celery worker.
     Will raise WorkerShutdown to kill the celery worker if the timeout is reached.
 
     Args:
         sender (Any): The sender of the signal.
-        **kwargs: Arbitrary keyword arguments.
+        **kwargs (Any): Additional keyword arguments.
     """
     probe_config = ProbeConfig()
     redis_client = RedisConnector().client
@@ -61,26 +69,30 @@ def wait_for_redis(sender: Any, **kwargs) -> None:
         raise
 
 
-def wait_for_db(sender: Any, **kwargs) -> None:
+def wait_for_db(sender: Any, **kwargs: Any) -> None:  # NOSONAR
     """
     Wait for the database to be available before starting the Celery worker.
     Will raise WorkerShutdown to kill the celery worker if the timeout is reached.
 
     Args:
         sender (Any): The sender of the signal.
-        **kwargs: Arbitrary keyword arguments.
+        **kwargs (Any): Additional keyword arguments.
     """
     probe_config = ProbeConfig()
 
     try:
         with timeout_context() as get_elapsed:
             while True:
-                # Check if the database server is ready
                 try:
-                    with get_db_session() as session:
-                        session.execute("SELECT 1")
-                        logger.info("Database: Readiness probe succeeded")
-                        break
+                    # Check if the database server is ready
+                    with Session(MSSQLConnector.get_engine()) as session:
+                        result = session.execute(text("SELECT 1")).scalar()
+                        if result:
+                            logger.info("Database: Readiness probe succeeded")
+
+                            # Close the session
+                            session.close()
+                            break
                 except Exception as e:
                     logger.warning(f"Database: Probe failed: {e}")
 
@@ -104,10 +116,14 @@ def wait_for_db(sender: Any, **kwargs) -> None:
         raise
 
 
-def wait_for_qdrant(sender: Any, **kwargs) -> None:
+def wait_for_qdrant(sender: Any, **kwargs: Any) -> None:  # NOSONAR
     """
     Wait for Qdrant to be available before starting the Celery worker.
     Will raise WorkerShutdown to kill the celery worker if the timeout is reached.
+
+    Args:
+        sender (Any): The sender of the signal.
+        **kwargs (Any): Additional keyword arguments.
     """
     probe_config = ProbeConfig()
     qdrant_connector = QdrantConnector()
@@ -141,3 +157,66 @@ def wait_for_qdrant(sender: Any, **kwargs) -> None:
             logger.error(f"Qdrant: Unexpected error during readiness probe: {e}")
             raise RedisProbeError(f"Probe failed with error: {e}") from e
         raise
+
+
+def on_worker_ready(sender: Any, **kwargs: Any) -> None:  # NOSONAR
+    """
+    Handles worker ready signal. This runs when the worker is ready to process tasks,
+    after initialization is complete.
+
+    Args:
+        sender (Any): The sender of the signal.
+        **kwargs (Any): Additional keyword arguments.
+    """
+    task_logger.info("Worker is ready to process tasks")
+
+
+def on_worker_shutdown(sender: Any, **kwargs: Any) -> None:  # NOSONAR
+    """
+    Handles worker shutdown signal. This runs when the worker is shutting down.
+
+    Args:
+        sender (Any): The sender of the signal.
+        **kwargs (Any): Additional keyword arguments.
+    """
+    task_logger.info("Worker is shutting down")
+
+
+def on_setup_logging(
+    loglevel: Any,
+    logfile: Any,
+    format: Any,
+    colorize: Any,
+    **kwargs: Any,  # NOSONAR
+) -> None:
+    """
+    Setup logging for the Celery worker.
+
+    Args:
+        loglevel (Any): The logging level.
+        logfile (Any): The file to log to.
+        format (Any): The log format.
+        colorize (Any): Whether to colorize the log.
+        **kwargs (Any): Additional keyword arguments.
+    """
+    # Configure the logging for the Celery logger
+    get_logger(
+        from_logger=logger,
+        log_level=loglevel,
+        log_to_file=True if logfile else False,
+        log_file_path=logfile,
+        console_formatter=ColoredFormatter,
+        file_formatter=PlainFormatter,
+        include_uvicorn=False,
+    )
+
+    # Configure the logging for the task logger
+    get_logger(
+        from_logger=task_logger,
+        log_level=loglevel,
+        log_to_file=True if logfile else False,
+        log_file_path=logfile,
+        console_formatter=CeleryTaskColoredFormatter,
+        file_formatter=CeleryTaskPlainFormatter,
+        include_uvicorn=False,
+    )
