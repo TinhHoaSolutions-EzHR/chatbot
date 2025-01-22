369ba51d93025c06a91353293d84f855c70137ab
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2025-01-22 12:37:32 +0700

    feat(frontend): Add draggable ChatSession button

--- chatbot-core/frontend/src/views/main-layout/app-sidebar/app-sidebar.tsx
+++ chatbot-core/frontend/src/views/main-layout/app-sidebar/app-sidebar.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { DndContext } from '@dnd-kit/core';
 
 import { Sidebar, SidebarContent, SidebarFooter, SidebarSeparator } from '@/components/ui/sidebar';
 
@@ -8,15 +9,17 @@ import { ChatHistory } from './chat-history/chat-history';
 
 export function AppSidebar() {
   return (
-    <Sidebar>
-      <AppSidebarHeader />
-      <SidebarSeparator className="mt-1" />
-      <SidebarContent>
-        <ChatFolders />
-        <SidebarSeparator />
-        <ChatHistory />
-      </SidebarContent>
-      <SidebarFooter />
-    </Sidebar>
+    <DndContext>
+      <Sidebar>
+        <AppSidebarHeader />
+        <SidebarSeparator className="mt-1" />
+        <SidebarContent>
+          <ChatFolders />
+          <SidebarSeparator />
+          <ChatHistory />
+        </SidebarContent>
+        <SidebarFooter />
+      </Sidebar>
+    </DndContext>
   );
 }
