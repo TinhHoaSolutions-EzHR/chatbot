3116801172a1d0500131ffc90f454f3bdf54b91f
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2025-01-22 15:28:21 +0700

    feat(frontend): Add move chat session to folder with dnd

--- chatbot-core/frontend/src/views/main-layout/app-sidebar/app-sidebar.tsx
+++ chatbot-core/frontend/src/views/main-layout/app-sidebar/app-sidebar.tsx
@@ -1,16 +1,41 @@
 'use client';
-import { DndContext } from '@dnd-kit/core';
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
 import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
+import React, { useState } from 'react';
+import { toast } from 'sonner';
 
 import { Sidebar, SidebarContent, SidebarFooter, SidebarSeparator } from '@/components/ui/sidebar';
+import { useEditChatSession } from '@/hooks/chat/use-edit-chat-session';
 
 import { AppSidebarHeader } from './app-sidebar-header';
 import { ChatFolders } from './chat-folders/chat-folders';
 import { ChatHistory } from './chat-history/chat-history';
 
 export function AppSidebar() {
+  const [, setIsDropped] = useState(false);
+  const { mutate } = useEditChatSession();
+
+  function handleDragEnd(event: DragEndEvent) {
+    if (event.over) {
+      setIsDropped(true);
+      mutate(
+        { chatSessionId: `${event.active.id}`, data: { folder_id: `${event.over.id}` } },
+        {
+          onSuccess() {
+            toast.success('Move chat successfully!');
+          },
+          onError() {
+            toast.error('Move chat failed.', {
+              description: "There's something wrong with your request. Please try again later!",
+            });
+          },
+        },
+      );
+    }
+  }
+
   return (
-    <DndContext modifiers={[restrictToVerticalAxis]}>
+    <DndContext modifiers={[restrictToVerticalAxis]} onDragEnd={handleDragEnd}>
       <Sidebar>
         <AppSidebarHeader />
         <SidebarSeparator className="mt-1" />
