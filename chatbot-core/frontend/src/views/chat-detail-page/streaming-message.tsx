6232431c1a6e51a4c66960b5a11ec7c2ff7371c9
greyy-nguyen <nhan.nguyen@skymavis.com>
2025-01-30 11:01:14 +0700

    fix(frontend): multi chat session misbehavior

--- chatbot-core/frontend/src/views/chat-detail-page/streaming-message.tsx
+++ chatbot-core/frontend/src/views/chat-detail-page/streaming-message.tsx
@@ -2,12 +2,16 @@ import React, { FC } from 'react';
 
 import { ChatMessage } from '@/components/chat-message/chat-message';
 import WillRender from '@/components/will-render';
+import { useChatStore } from '@/hooks/stores/use-chat-store';
 import { ChatMessageType, StreamingMessageState } from '@/types/chat';
 
-import { useStreamingChatContext } from './providers/streaming-chat-provider';
+interface IStreamingMessageProps {
+  chatSessionId: string | null;
+}
 
-export const StreamingMessage: FC = () => {
-  const { streamingMessage, streamState } = useStreamingChatContext();
+export const StreamingMessage: FC<IStreamingMessageProps> = ({ chatSessionId }) => {
+  const streamingMessage = useChatStore(state => state.chatSession[chatSessionId ?? '']?.streamingMessage);
+  const streamState = useChatStore(state => state.chatSession[chatSessionId ?? '']?.streamState);
 
   return (
     <WillRender when={streamState !== StreamingMessageState.IDLE}>
