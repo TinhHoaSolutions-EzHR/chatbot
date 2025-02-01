18c517632431f665d8ddfa14cc9475ea369d7ae7
greyy-nguyen <nhan.nguyen@skymavis.com>
2025-02-01 10:24:04 +0700

    fix(frontend): add message to client state after aborting

--- chatbot-core/frontend/src/hooks/chat/use-create-chat-message.ts
+++ chatbot-core/frontend/src/hooks/chat/use-create-chat-message.ts
@@ -3,6 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 import { ReactMutationKey } from '@/constants/react-query-key';
 import { createChatMessage } from '@/services/chat/create-chat-message';
 import { ChatMessageStreamEvent, ChatMessageType, StreamingMessageState } from '@/types/chat';
+import { checkAbortError } from '@/utils/check-error';
 import { decodeChatStreamChunk } from '@/utils/decode-chat-stream-chunk';
 
 import { useChatStore } from '../stores/use-chat-store';
@@ -22,59 +23,61 @@ export const useCreateChatMessage = ({ chatSessionId }: IUseCreateChatMessagePro
   return useMutation({
     mutationKey: [ReactMutationKey.CREATE_CHAT_MESSAGE, chatSessionId],
     mutationFn: async (props: Omit<ChatMessageRequest, 'abortController'>) => {
-      const abortController = new AbortController();
-      const stream = await createChatMessage({ ...props, abortController });
-      const reader = stream.pipeThrough(new TextDecoderStream()).getReader();
-      setStreamAbortController(props.chatSessionId, abortController);
-
       let fullResponse = '';
-      let isCompleteMessage = false;
 
-      while (true) {
-        const { value, done } = await reader.read();
-        if (done) {
-          if (!isCompleteMessage) {
-            addChatMessage(props.chatSessionId, {
-              id: window.crypto.randomUUID(),
-              message: fullResponse,
-              chat_session_id: props.chatSessionId,
-              message_type: ChatMessageType.ASSISTANT,
-              is_sensitive: false,
-              created_at: new Date().toLocaleDateString(),
-              updated_at: new Date().toLocaleDateString(),
-              deleted_at: null,
-            });
+      try {
+        const abortController = new AbortController();
+        const stream = await createChatMessage({ ...props, abortController });
+        const reader = stream.pipeThrough(new TextDecoderStream()).getReader();
+        setStreamAbortController(props.chatSessionId, abortController);
+
+        while (true) {
+          const { value, done } = await reader.read();
+
+          if (done) {
+            break;
           }
 
-          break;
+          const { event, data } = decodeChatStreamChunk(value);
+
+          switch (event) {
+            case ChatMessageStreamEvent.METADATA:
+              addChatMessage(props.chatSessionId, data);
+              setStreamState(props.chatSessionId, StreamingMessageState.PENDING);
+              break;
+            case ChatMessageStreamEvent.DELTA:
+              if (!fullResponse) {
+                setStreamState(props.chatSessionId, StreamingMessageState.STREAMING);
+              }
+
+              const newChunk = data;
+              fullResponse += newChunk;
+              setStreamingMessage(props.chatSessionId, fullResponse);
+              break;
+            case ChatMessageStreamEvent.STREAM_COMPLETE:
+              const serverChatResponse = {
+                ...data,
+                message: fullResponse,
+              };
+              addChatMessage(props.chatSessionId, serverChatResponse);
+              setStreamState(props.chatSessionId, StreamingMessageState.IDLE);
+              break;
+          }
         }
-
-        const { event, data } = decodeChatStreamChunk(value);
-
-        switch (event) {
-          case ChatMessageStreamEvent.METADATA:
-            addChatMessage(props.chatSessionId, data);
-            setStreamState(props.chatSessionId, StreamingMessageState.PENDING);
-            break;
-          case ChatMessageStreamEvent.DELTA:
-            if (!fullResponse) {
-              setStreamState(props.chatSessionId, StreamingMessageState.STREAMING);
-            }
-
-            const newChunk = data;
-            fullResponse += newChunk;
-            setStreamingMessage(props.chatSessionId, fullResponse);
-            break;
-          case ChatMessageStreamEvent.STREAM_COMPLETE:
-            const serverChatResponse = {
-              ...data,
-              message: fullResponse,
-            };
-            addChatMessage(props.chatSessionId, serverChatResponse);
-            setStreamState(props.chatSessionId, StreamingMessageState.IDLE);
-            isCompleteMessage = true;
-            break;
+      } catch (error) {
+        if (checkAbortError(error)) {
+          addChatMessage(props.chatSessionId, {
+            id: window.crypto.randomUUID(),
+            message: fullResponse,
+            chat_session_id: props.chatSessionId,
+            message_type: ChatMessageType.ASSISTANT,
+            is_sensitive: false,
+            created_at: new Date().toLocaleDateString(),
+            updated_at: new Date().toLocaleDateString(),
+            deleted_at: null,
+          });
         }
+        throw error;
       }
     },
     onSettled(_, __, variables) {
