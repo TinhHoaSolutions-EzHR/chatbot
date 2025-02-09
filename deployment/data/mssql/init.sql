a9199dbd21264359c468464fe3c344bcddc01a32
tuantran0910 <tntuan0910@gmail.com>
2025-02-09 11:06:09 +0700

    feat(database): set transaction isolation level for chatbot_core database

--- deployment/data/mssql/init.sql
+++ deployment/data/mssql/init.sql
@@ -1,3 +1,8 @@
 -- Create a new database
 CREATE DATABASE chatbot_core;
 CREATE DATABASE vanphongso;
+
+-- Set the transaction isolation level for the database
+ALTER DATABASE chatbot_core
+SET READ_COMMITTED_SNAPSHOT ON
+WITH ROLLBACK IMMEDIATE;
